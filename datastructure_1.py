1c55d37b8dcf645c9cd72f5c131feb852f1db2ed
maxenceryan <53180223+maxenceryan@users.noreply.github.com>
2019-10-30 23:00:18 +0100

    submission 2

--- datastructure_1.py
+++ datastructure_1.py
@@ -16,6 +16,7 @@ boundary_vertices = [item for sublist in l for item in sublist if mesh.vertex_de
 start_vertex_index = random.randint(0,len(boundary_vertices)-1)
 start_vertex_key = boundary_vertices[start_vertex_index]
 
+# only for quadmesh
 def get_traverse(start_key):
 
     if start_key not in boundary_vertices:
@@ -23,27 +24,31 @@ def get_traverse(start_key):
     if mesh.vertex_degree(start_key) == 2:
         raise ValueError('Vertex cannot be corner')
     
-    start_neighbors = mesh.vertex_neighbors(start_key)
-    # find the index that corresponds to the next vertex for traversing the mesh
-    # this helps ensure we go the proper direction despite the algorithm for ordering neighboring vertices
-    index_traverse = [mesh.vertex_degree(key) for key in start_neighbors] #if mesh.vertex_degree(start_key)==4]
-    #print(index_traverse)
-    index_traverse = index_traverse[0]
-
     vertices = [start_key]
-    current = start_neighbors[index_traverse]
+    for key in mesh.vertex_neighbors(start_key):
+        if mesh.vertex_degree(key) == 4:
+            current = key
+            break
+
+    previous = start_key
 
     while True:
         vertices.append(current)
         if mesh.vertex_degree(current) < 4:
             break
-        current = mesh.vertex_neighbors(current)[index_traverse]
+        
+        i = mesh.vertex_neighbors(current).index(previous)
 
-    edges = []
+        if i+2 >= 3:
+            i = (i+2) % 4
 
-    return vertices, edges
+        i = i+2 % 4
 
-traverse_vertices, traverse_edges = get_traverse(start_vertex_key)
+        current = mesh.vertex_neighbors(current)[i]
+
+    return vertices
+
+traverse_vertices = get_traverse(start_vertex_key)
 #print(traverse_vertices)
 
 plotter.draw_vertices(
@@ -53,7 +58,7 @@ plotter.draw_vertices(
 )
 
 plotter.draw_edges(
-    color={key: (255,0,0) for key in traverse_edges},
+    #color={key: (255,0,0) for key in traverse_edges},
 )
 
 plotter.show()
